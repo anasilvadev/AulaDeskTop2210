4876f0ddb87aefba84813a3e77df67c7190f89e4
Ana Clara Silva <ana.silva.ifpr.2023@gmail.com>
2025-10-22 15:58:42 -0300

    Add files via upload

---
+++ js/exe00.js
@@ -0,0 +1,36 @@
+var  elemento
+elemento= document.getElementById("titulo")
+/*alert(elemento)*/
+/*Exemplo 2  */
+function Mudartitulo(){
+    var titulo
+    titulo = documento.getElementById("titulo")
+    titulo.innerHTML= "Alterando o título via js"
+}
+
+/*Exemplo 3 */
+function mudarestilo(){
+    var titulo 
+    titulo = document.getElementById("titulo")
+    titulo.style.color="brown"
+    titulo.style.backgroundColor ="beige"
+    titulo.style.fontSize="68px"
+}
+
+/*Exemplo 4 */
+function destacar(){
+var titulo= document.getElementById("titulo")
+titulo.classList.add("destacado")
+}
+
+/*Exemplo 5 */
+function remover(){
+    var titulo=  document.getElementById("titulo")
+    titulo.classList.remove("destacado")
+}
+
+/*Exemplo 6 */
+function alternar(){
+    var titulo=  document.getElementById("titulo")
+    titulo.classList.toggle("destacado")
+}
